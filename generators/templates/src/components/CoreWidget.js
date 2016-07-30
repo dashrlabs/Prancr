51e727968c75569d42c8c5446ba0431d4e3ff493
Michael Hoang <michael.hoang@wspdigital.com>
2016-07-30 17:25:15 +1000

    Add identifiers

--- generators/templates/src/components/CoreWidget.js
+++ generators/templates/src/components/CoreWidget.js
@@ -6,6 +6,10 @@ export default class MyNewWidget extends React.Component {
     settings: React.PropTypes.object.isRequired,
   };
 
+  static id = 'your-widget-id';
+  static widgetName = 'your-widget-name';
+  static sizes = [[1, 1]];
+
   render() {
     return (
       <div>Hello World - This is your new widget</div>
